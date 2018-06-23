12455f71488c68b6395060989c219aa2ed3b140b
csyang79 <csyang79@gmail.com>
2018-06-23 22:10:48 +0800

    add alloc

---
+++ alloc.cpp
@@ -0,0 +1,100 @@
+#include "alloc.h"
+
+namespace TinySTL
+{
+	char *alloc::_start_free = 0;
+	char *alloc::_end_free = 0;
+	size_t alloc::_heap_size = 0;
+
+	alloc::obj *alloc::_free_list[alloc::_NFREELISTS] = 
+		{ 0,  0,  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0,  0,  0 };
+
+	void *_refill(size_t _n)
+	{
+		int _nobjs = 20;
+		char *_chunk = _chunk_alloc(_n, _nobjs);
+		_obj* volatile *_my_free_list;
+		_obj* _result;
+		_obj* _curr_obj;
+		_obj* _next_obj;
+		int _i;
+
+		if (_nobjs == 1)
+			return _chunk;
+		_my_free_list = _free_list + _free_list_index(_n);
+		_result = (_obj*)_chunk;
+		*_my_free_list = _next_obj = (_obj*)(_chunk + _n);
+		for (_i = 1; ; ++i)
+		{
+			_curr_obj = _next_obj;
+			_next_obj = (_obj*)((char*)_next_obj + _n);
+			if (_nobjs - 1 == _i)
+			{
+				_curr_obj->_next = nullptr;
+				break;
+			}
+			else
+			{
+				_current_obj->_next = _next_obj;
+			}
+		}
+		return _result;
+	}
+	
+	char *alloc::_chunk_alloc(size_t _size, size_t &_nobjs)
+	{
+		char *_result;
+		size_t _total_bytes = _size * _nobjs;
+		size_t _bytes_left = _end_free - _start_free;
+		if (_bytes_left >= _total_bytes)
+		{
+			_result = _start_free;
+			_start_free += _total_bytes;
+			return _result;
+		}
+		else if (_bytes_left >= _size)
+		{
+			_nobjs = (int)(_bytes_left / _size);
+			_total_bytes = _nobjs * _size;
+			_result = _start_free;
+			_start_free += _total_bytes;
+			return _result;
+		}
+		else
+		{
+			size_t _bytes_to_get = 2 * _total_bytes + _round_up(_heap_size >> 4);
+			if (_bytes_left > 0)
+			{
+				_obj *volatile *_my_free_list = _free_list + _free_list_index(_bytes_left);
+				(_obj*)_start_free->_next = *_my_free_list;
+				*_my_free_list = (_obj*)_start_free;
+			}
+			
+			_start_free = (char*)malloc(_bytes_to_get);
+			if (_start_free == nullptr)
+			{
+				size_t _i;
+				_obj *volatile *_my_free_list;
+				_obj *_p;
+				for (_i = _size; _i <= (size_t)_MAX_BYTES; _i += _ALIGN)
+				{
+					_my_free_list = _free_list + _free_list_index(_i);
+					_p = *_my_free_list;
+					if (_p != nullptr)
+					{
+						*_my_free_list = _p->_next;
+						_start_free = (char*)_p;
+						_end_free = _start_free + _i;
+						return _chunk_alloc(_size, _nobjs);
+					}
+				}
+				_end_free = nullptr;
+				
+			}
+			_heap_size += _bytes_to_get;
+			_end_free = _start_free + _bytes_to_get;
+			return _chunk_alloc(_size, _nobjs);
+		}
+	}
+}
+
---
+++ alloc.h
@@ -0,0 +1,90 @@
+#ifndef _ALLOC_H
+#define _ALLOC_H
+
+#include <cstdlib>
+
+namespace TinySTL 
+{
+	class alloc
+	{
+	private:
+		enum { _ALIGN = 8 };
+		enum { _MAX_BYTES = 128 };
+		enum { _NFREELISTS = 16 };
+	
+	private:
+		union _obj
+		{
+			union obj *_next;
+			char _client[1];
+		};
+
+		static obj *_free_list[_NFREELISTS];
+	
+
+	private:
+		static size_t _round_up(size_t _bytes)
+		{
+			return (_bytes + (size_t)_ALIGN - 1) & ~((size_t)_ALGIN - 1);
+		}
+
+		static size_t _free_list_index(size_t _bytes)
+		{
+			return (_bytes + (size_t)_ALIGN - 1) / (size_t)_ALIGN - 1;
+		}
+		
+		static void *_refill(size_t _n);
+		static char *_chunk_alloc(size_t _n, int &_nobjs);
+	
+	private:
+		static char *_start_free;
+		static char *_end_free;
+
+		static size_t _heap_size;
+	
+	public:
+		static void *allocate(size_t _n)
+		{
+			void *_ret =  0;
+			if (_n > (size_t)_MAX_BYTES
+			{
+				return malloc(_n);
+			}
+			else
+			{
+				_obj* volatile *_my_free_list = _free_list + 
+					_free_list_index(_n);
+				_obj* _result = *_my_free_list;
+				if (result == nullptr)
+					_ret = _refill(_round_up(_n));
+				else
+				{
+					*my_free_list = result->_next;
+					_ret = _result;
+				}
+			}
+			return _ret;
+		}
+	
+		static void deallocate(void *_p, size_t _n)
+		{
+			if (_n > _MAX_BYTES)
+				free(p);
+			else
+			{
+				_obj* volatile *_my_free_list = _free_list + _free_list_index(_n);
+				_obj* _q = (_obj*) _p;
+				_q->_next = *_my_free_list;
+				*_my_free_list = _q;
+			}
+		}
+
+		void *reallocate(void *_p, size_t _old_size, size_t _new_size)
+		{
+			deallocate(_p, _old_size);
+			return allocate(_p, _new_size);
+		}
+	};
+}
+
+#endif
