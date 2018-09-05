b9769cfc81ab060e53f11d6fea1be5946c33b2f8
csyang79 <csyang79@gmail.com>
2018-09-05 13:18:09 +0800

    add algorithm and functional

---
+++ algorithm.h
@@ -0,0 +1,16 @@
+#ifndef _ALGORITHM_H
+#define _ALGORITHM_H
+
+#include <cstring>
+#include <utility>
+
+#include "functional.h"
+#include "allocator.h"
+#include "iterator.h"
+#include "utility.h"
+namespace TinySTL
+{
+	
+}
+
+#endif
